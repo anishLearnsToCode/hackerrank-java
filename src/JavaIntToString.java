34304b99805ae1fb28852a1e162a62ed97861ce1
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-12 14:52:35 +0530

    solves java int to sting

---
+++ src/JavaIntToString.java
@@ -0,0 +1,13 @@
+// https://www.hackerrank.com/challenges/java-int-to-string/problem
+
+import java.util.Scanner;
+
+public class JavaIntToString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        String n = number + "";
+
+
+    }
+}
