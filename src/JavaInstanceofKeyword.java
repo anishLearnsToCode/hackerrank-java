e821b621f1886656be6d78f719b3ee994b6c8578
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-13 17:35:42 +0530

    solves java instanceof keyword

---
+++ src/JavaInstanceofKeyword.java
@@ -0,0 +1,38 @@
+// https://www.hackerrank.com/challenges/java-instanceof-keyword/problem
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class JavaInstanceofKeyword {
+    public static void main(String[] args) {
+        ArrayList mylist = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0; i<t; i++){
+            String s=sc.next();
+            if(s.equals("Student"))mylist.add(new Student());
+            if(s.equals("Rockstar"))mylist.add(new Rockstar());
+            if(s.equals("Hacker"))mylist.add(new Hacker());
+        }
+        System.out.println(count(mylist));
+    }
+
+    private static String count(ArrayList mylist){
+        int a = 0,b = 0,c = 0;
+        for(int i = 0; i < mylist.size(); i++){
+            Object element=mylist.get(i);
+            if(element instanceof Student)
+                a++;
+            if(element instanceof Rockstar)
+                b++;
+            if(element instanceof Hacker)
+                c++;
+        }
+        String ret = Integer.toString(a)+" "+ Integer.toString(b)+" "+ Integer.toString(c);
+        return ret;
+    }
+}
+
+class Student {}
+class Rockstar {}
+class Hacker {}
