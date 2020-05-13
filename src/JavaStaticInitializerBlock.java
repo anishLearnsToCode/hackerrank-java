8619284cfdbf3121925705d322235b56e201d777
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-13 11:34:08 +0530

    solves Static initializer bock

--- src/JavaStaticInitializerBlock.java
+++ src/JavaStaticInitializerBlock.java
@@ -11,7 +11,7 @@ public class JavaStaticInitializerBlock {
         Scanner scanner = new Scanner(System.in);
         B = scanner.nextInt();
         H = scanner.nextInt();
-        if (B < 0 || H < 0) {
+        if (B <= 0 || H <= 0) {
             flag = false;
             System.out.println("java.lang.Exception: Breadth and height must be positive");
         }
---
+++ src/JavaStringsIntroduction.java
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/java-strings-introduction/problem
+
+import java.util.Scanner;
+
+public class JavaStringsIntroduction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.nextLine();
+        String b = scanner.nextLine();
+
+        System.out.println(a.length() + b.length());
+        System.out.println(a.compareTo(b) > 0 ? "Yes" : "No");
+        System.out.println(capitalize(a) + " " + capitalize(b));
+    }
+
+    private static String capitalize(String string) {
+     stat   return string.substring(0, 1).toUpperCase() + string.substring(1);
+    }
+}
