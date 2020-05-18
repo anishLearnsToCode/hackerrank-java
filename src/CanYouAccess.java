f148b9ab2f366d8df0bfba683063072961d1d5d6
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-19 02:36:58 +0530

    adds solution to ca you access ?

---
+++ src/CanYouAccess.java
@@ -0,0 +1,8 @@
+// https://www.hackerrank.com/challenges/can-you-access/problem
+
+public class CanYouAccess {
+    public static void main(String[] args) {
+//        o = (new Inner()).new Private();
+//        System.out.println(num + " is " + ((Solution.Inner.Private)o).powerof2(num));
+    }
+}
