46bd804d66e56592c052dcc6be9680ec9d7c3186
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-05-20 16:16:40 +0530

    solves java annotations

---
+++ src/JavaAnnotations.java
@@ -0,0 +1,63 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
+public class JavaAnnotations {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String role = in.next();
+            int spend = in.nextInt();
+            try {
+                Class annotatedClass = FamilyMember.class;
+                Method[] methods = annotatedClass.getMethods();
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(FamilyBudget.class)) {
+                        FamilyBudget family = method
+                                .getAnnotation(FamilyBudget.class);
+                        String userRole = family.userRole();
+                        int budgetLimit = family.budgetLimit();
+                        if (userRole.equals(role)) {
+                            if(spend <= budgetLimit){
+                                method.invoke(FamilyMember.class.newInstance(),
+                                        budgetLimit, spend);
+                            }else{
+                                System.out.println("Budget Limit Over");
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            testCases--;
+        }
+    }
+
+    private static class FamilyMember {
+        @FamilyBudget(userRole="SENIOR")
+        public void seniorMember(int budget, int moneySpend) {
+            System.out.println("Senior Member");
+            System.out.println("Spend: " + moneySpend);
+            System.out.println("Budget Left: " + (budget - moneySpend));
+        }
+
+        @FamilyBudget(userRole="JUNIOR", budgetLimit=50)
+        public void juniorUser(int budget, int moneySpend) {
+            System.out.println("Junior Member");
+            System.out.println("Spend: " + moneySpend);
+            System.out.println("Budget Left: " + (budget - moneySpend));
+        }
+    }
+
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    private @interface FamilyBudget {
+        String userRole() default "GUEST";
+        int budgetLimit() default 100;
+    }
+}
